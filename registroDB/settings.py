3f46e4d4d61372390eb6afe8287aea6a2ac0f228
luis5258 <larellano0516@gmail.com>
2021-08-11 21:22:33 -0700

    proyecto

--- registroDB/settings.py
+++ registroDB/settings.py
@@ -69,23 +69,23 @@ WSGI_APPLICATION = 'registroDB.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    #  'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3', 
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3', 
   
-    # }
-   'default': {
-    'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '12345',
-        'NAME': 'dbweb',
-        'OPTIONS': {
-        'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"}
+    }
+#    'default': {
+#     'ENGINE': 'django.db.backends.mysql',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'USER': 'root',
+#         'PASSWORD': '12345',
+#         'NAME': 'dbweb',
+#         'OPTIONS': {
+#         'init_command': "SET sql_mode = 'STRICT_TRANS_TABLES'"}
   
         
-   }
+#    }
 }
 # 
  #
